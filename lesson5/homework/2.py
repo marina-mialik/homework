984d18b66088939a7ccb07c6201c7d1ce667c93b
Marina <marina.mialik@gmail.com>
2025-02-02 22:07:19 +0300

    2.py

--- lesson5/homework/2.py
+++ lesson5/homework/2.py
@@ -43,11 +43,11 @@ employee_name = input("Введите ФИО сотрудника: ")
 employee = employees[employee_name]
 print(f"Информация о сотруднике: {employee_name}")
 print(f"Должность: {employee['должность']}")
-print(f"Год рождения: {employee['год_рождения']}")
+print(f"Год рождения: {employee['год рождения']}")
 print("Навыки:")
 for skill in employee["навыки"]:
   print(f"    - {skill}")
 print("Дети:")
 if employee["дети"]:
   for child in employee["дети"]:
-   print(f"    - {child['имя']}, год рождения: {child['год_рождения']}")
+   print(f"    - {child['имя']}, год рождения: {child['год рождения']}")
