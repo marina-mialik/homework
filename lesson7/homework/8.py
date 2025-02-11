81b02173effaa309488c2271492ef8cb8c86712d
Marina Mialik <marina.mialik@gmail.com>
2025-02-11 13:52:42 +0300

    lesson7 done(8-9)

--- lesson7/homework/8.py
+++ lesson7/homework/8.py
@@ -14,3 +14,96 @@
     Неправильный формат. Пример: '2 + 4'
 
 """
+
+operations = {
+    'sum': "+",
+    'sub': "-",
+    'mul': "*",
+    'div': "/",
+    'pow': "**"
+}
+
+def set_warnings(a: str | None, b: str | None, operation: str) -> str:
+    if not a or not b:
+        return f"Неправильный формат. Пример: '2 {operation} 4'."
+    
+    a, b = a.replace('.', '', 1), b.replace('.', '', 1)
+
+    if not a.isdigit() or not b.isdigit():
+        return "Некорректные числа."
+    
+    return ""
+
+
+def get_sum(a: str | None, b: str | None) -> str:
+    warnings = set_warnings(a, b, operations['sum'])
+
+    if len(warnings):
+        return warnings
+
+    a, b = int(a), int(b)
+    return a + b
+
+def get_sub(a: str | None, b: str | None) -> str:
+    warnings = set_warnings(a, b, operations['sub'])
+
+    if len(warnings):
+        return warnings
+    
+    a, b = int(a), int(b)
+    return a - b
+
+def get_mul(a: str | None, b: str | None) -> str:
+    warnings = set_warnings(a, b, operations['mul'])
+
+    if len(warnings):
+        return warnings
+
+    a, b = int(a), int(b)
+    return a * b
+
+def get_div(a: str | None, b: str | None) -> str:
+    warnings = set_warnings(a, b, operations['div'])
+
+    if len(warnings):
+        return warnings
+
+    a, b = int(a), int(b)
+    
+    if b == 0:
+        return 'На ноль делить нельзя'
+
+    return a / b
+
+def get_pow(a: str | None, b: str | None) -> str:
+    warnings = set_warnings(a, b, operations['pow'])
+
+    if len(warnings):
+        return warnings
+
+    a, b = int(a), int(b)
+    return a ** b
+
+while True:
+    example = input("Введите пример или 'стоп' для завершения: ").replace(' ', '')
+
+    if example.lower() == "стоп":
+        break
+
+    if operations['sum'] in example:
+        a, b = example.split(operations['sum'])
+        print(f"Ответ: {get_sum(a, b)}")
+    elif operations['sub'] in example:
+        a, b = example.split(operations['sub'])
+        print(f"Ответ: {get_sub(a, b)}")
+    elif operations['pow'] in example:
+        a, b = example.split(operations['pow'])
+        print(f"Ответ: {get_pow(a, b)}")
+    elif operations['mul'] in example:
+        a, b = example.split(operations['mul'])
+        print(f"Ответ: {get_mul(a, b)}")
+    elif operations['div'] in example:
+        a, b = example.split(operations['div'])
+        print(f"Ответ: {get_div(a, b)}")
+    else:
+        print("Неизвестная операция.")
--- lesson7/homework/9.py
+++ lesson7/homework/9.py
@@ -22,6 +22,73 @@
     | 3 | Базы данных | Николаев Е.Н.   |     74     |
     ...
 
- 
-
 '''
+
+employees = {
+    "1": {
+        "fullname": "Иванов Иван Иванович",
+        "position": "Дизайнер",
+        "birthday": 1992,
+        "skills": [
+            { "skill_name": "Illustrator", "mastery": 65 },
+            { "skill_name": "Corel Draw", "mastery": 80 },
+            { "skill_name": "Photoshop", "mastery": 40 },
+        ],
+        "children": [
+            {"name": "Вероника", "birthday": 2017},
+            {"name": "Михаил", "birthday": 2020},
+        ],
+    },
+    "2": {
+        "fullname": "Петров Петр Петрович",
+        "position": "Юрист",
+        "birthday": 2000,
+        "skills": [
+            { "skill_name": "Exel", "mastery": 65 },
+            { "skill_name": "Word", "mastery": 80 },
+            { "skill_name": "Консультант Плюс", "mastery": 40 },
+            { "skill_name": "Английский", "mastery": 40 },
+        ],
+        "children": [
+            {"name": "Лидия", "birthday": 2022},
+        ],
+    },
+    "3": {
+        "fullname": "Сидоров Василий Владимирович",
+        "position": "Программист",
+        "birthday": 1990,
+        "skills": [
+            { "skill_name": "Python", "mastery": 32 },
+            { "skill_name": "JavaScript", "mastery": 54 },
+            { "skill_name": "Django", "mastery": 44 },
+            { "skill_name": "Git", "mastery": 90 },
+        ],
+        "children": [
+            {"name": "Мария", "birthday": 2013},
+        ],
+    }   
+}
+
+workers = []
+
+for worker in employees.values():
+    first_name, second_name, middle_name = worker["fullname"].split()
+    name = f"{second_name} {first_name[0]}.{middle_name[0]}."
+    top_skill_name = ""
+    top_mastery = 0
+
+    for skill in worker["skills"]:
+        skill_name, mastery = skill["skill_name"], skill["mastery"]
+
+        if mastery >= top_mastery:
+            top_mastery = mastery
+            top_skill_name = skill_name
+    
+    result = {}
+    result["skill"] = top_skill_name
+    result["mastery"] = top_mastery
+    result["name"] = second_name
+    workers.append(result)
+
+for idx, worker in enumerate(workers):
+    print(f"{idx + 1}. {worker["skill"]} - {worker["name"]} - {worker["mastery"]}")
