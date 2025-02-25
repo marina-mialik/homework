b003e0797cc00b71bffe074e6cb20840715cfe5b
Marina <marina.mialik@gmail.com>
2025-02-25 16:38:44 +0300

    lesson12 done

--- lesson12/homework/3.py
+++ lesson12/homework/3.py
@@ -21,66 +21,6 @@
     * - stat, возвращает среднее количество изменений счетчика в секунду
 
 """
-# import time
-
-# class Counter:
-#     def __init__(self, start_value: int = 0, min_value: int = -float('inf'), max_value: int = float('inf')):
-#         self._value = start_value
-#         self._start_value = start_value
-#         self._min_value = min_value
-#         self._max_value = max_value
-#         self._changes = 0
-#         self._start_time = time.time()
-
-#     @property
-#     def value(self) -> int:
-#         return self._value
-
-#     def increase(self, num: int = 1):
-#         self._value = min(self._value + num, self._max_value)
-#         self._changes += 1
-
-#     def decrease(self, num: int = 1):
-#         self._value = max(self._value - num, self._min_value)
-#         self._changes += 1
-
-#     def reset(self):
-#         self._value = self._start_value
-#         self._changes += 1
-
-#     def __iter__(self):
-#         return self
-
-#     def __next__(self):
-#         if self._value >= self._max_value:
-#             raise StopIteration
-#         self.increase()
-#         return self._value
-
-#     def stat(self) -> float:
-#         elapsed_time = time.time() - self._start_time
-#         return self._changes / elapsed_time if elapsed_time > 0 else 0
-
-# counter = Counter(start_value=5, min_value=0, max_value=10)
-
-# print("Текущее значение:", counter.value)
-
-# counter.increase(3)
-# print("После увеличения на 3:", counter.value)
-
-# counter.decrease(2)
-# print("После уменьшения на 2:", counter.value)
-
-# counter.reset()
-# print("После сброса:", counter.value)
-
-# print("Итерация:")
-# for value in counter:
-#     print(value, end=" ")
-# print()
-
-# print("Среднее количество изменений в секунду:", counter.stat())
-
 
 import time
 
